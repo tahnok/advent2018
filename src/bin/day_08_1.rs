5464d330e82cd813de55140a8192b58d3723cbda
Wesley Ellis <tahnok@gmail.com>
2018-12-10 22:39:04 -0500

    better day 8 part 1

--- src/bin/day_08_1.rs
+++ src/bin/day_08_1.rs
@@ -8,25 +8,9 @@ fn main() {
 }
 
 fn calculate_checksum(input: &str) -> usize {
-    let mut checksum = 0;
     let mut nodes: Vec<usize> = input.trim().split(" ").map(|x| x.parse().unwrap()).collect();
     nodes.reverse();
-
-
-    while !nodes.is_empty() {
-        let mut childs = nodes.pop().unwrap();
-        let mut metadatas = nodes.pop().unwrap();
-        while childs > 0 {
-            childs -= 1;
-            checksum += recurse(&mut nodes);
-        }
-        while metadatas > 0 {
-            metadatas -= 1;
-            checksum += nodes.pop().unwrap();
-        }
-    }
-
-    checksum
+    recurse(&mut nodes)
 }
 
 fn recurse(nodes: &mut Vec<usize>) -> usize {
